d4c7e9fee77d9d039910ef77b284b0750f204513
Admin <admin@mac-admin.local>
2017-04-09 20:55:32 +0300

    create header file for managers

---
+++ iFlightControl/Logic/Managers/FCManagersHeader.h
@@ -0,0 +1,14 @@
+//
+//  FCManagersHeader.h
+//  iFlightControl
+//
+//  Created by Admin on 09.04.17.
+//  Copyright © 2017 MohylaUniversity. All rights reserved.
+//
+
+#ifndef FCManagersHeader_h
+#define FCManagersHeader_h
+
+#import "FCBluetoothManager.h"
+
+#endif /* FCManagersHeader_h */
